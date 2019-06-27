9058129b53e34b2dd8226d9b65acede970111187
noenemy <noenemy@gmail.com>
2019-06-27 15:41:57 +0900

    working on testing whatsnew crawler

--- lambda-api/crawler.py
+++ lambda-api/crawler.py
@@ -1,3 +1,29 @@
-'''
-initial commit
-'''
+import sys
+import urllib2
+
+url = "https://aws.amazon.com/about-aws/whats-new/2018/11/"
+whatsnew_page = urllib2.urlopen(url)
+
+page_source = whatsnew_page.read()
+
+elements = page_source.split('<')
+
+prevUrl = ''
+
+for element in elements:
+    if element.startswith('a href'):
+        
+        newUrl = element[7:]
+        if newUrl.startswith('"//aws.amazon.com/about-aws/whats-new'):
+            cutoff ='">'
+            newUrl = newUrl.split(cutoff, 1)[0]
+            cutoff = '" '
+            newUrl = newUrl.split(cutoff, 1)[0]
+            newUrl = newUrl[1:]
+            
+            if prevUrl != newUrl:
+                print newUrl
+                prevUrl = newUrl
+    else:
+        pass
+    
